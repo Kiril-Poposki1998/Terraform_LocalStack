9ecc87f102d7321ff54e59d2d70d5196a2f96ea7
KIRIL <kirilpoposki@hotmail.com>
2023-04-25 10:07:09 +0200

    format code, add bucket

--- instances.tf
+++ instances.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "instance-1" {
   instance_type = "t3.micro"
-  ami = var.ami_id
-  subnet_id = aws_subnet.sub_main_1.id
-  private_ip = "10.0.1.2"
+  ami           = var.ami_id
+  subnet_id     = aws_subnet.sub_main_1.id
+  private_ip    = "10.0.1.2"
 
   tags = {
     "Name" = "instance-1"
--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "sub_main_1" {
-    vpc_id     = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    tags = {
-        Name = "Main"
-    }
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Main"
+  }
 }
